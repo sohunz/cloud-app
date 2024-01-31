1afec7b69a90fd5df86192ff8ae62d275edf84b4
sohunz <sohunily@gmail.com>
2024-01-31 12:40:45 +0700

    responsive footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,11 @@ import { FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
     return (
-        <div className="mt-32 border bg-blue-950 text-white">
+        <div className="mt-32 border bg-blue-950 text-white px-10">
+        <div>
             <div className="max-w-[1440px] mx-auto flex flex-col">
-                <div className="grid grid-cols-5 py-10">
-                    <div className="flex flex-col gap-2">
+                <div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-2 md:gap-5 gap-10 py-10">
+                    <div className="flex flex-col gap-2 items-center">
                         <p>SOLUTIONS</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
@@ -17,7 +18,7 @@ const Footer = () => {
                         <p>Marketing</p>
                         <p>Marketing</p>
                     </div>
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 items-center">
                         <p>SOLUTIONS</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
@@ -25,7 +26,7 @@ const Footer = () => {
                         <p>Marketing</p>
                         <p>Marketing</p>
                     </div>
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 items-center">
                         <p>SOLUTIONS</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
@@ -33,46 +34,30 @@ const Footer = () => {
                         <p>Marketing</p>
                         <p>Marketing</p>
                     </div>
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 items-center">
                         <p>SOLUTIONS</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
                         <p>Marketing</p>
-                    </div>
-                    <div className="flex flex-col gap-3">
-                        <p>SUBSCRIBE TO OUR NEWSLETTER</p>
-                        <p>
-                            The latest news, articles, and resources, sent to
-                            your inbox weekly.
-                        </p>
-                        {/* <div className="flex flex-row items-center gap-2">
-                            <input
-                                type="email"
-                                placeholder="Enter Email"
-                                className="pl-3 py-2 rounded-md text-black outline-none"
-                            />
-                            <button className="bg-blue-500 px-2 py-2 rounded-md">
-                                Subscript
-                            </button>
-                        </div> */}
                     </div>
                 </div>
                 <hr className="w-full h-[2px] bg-gray-200 border-0 dark:bg-gray-700" />
-                <div className="flex items-center justify-between py-10">
+                <div className="flex lg:flex-row md:flex-row sm:flex-row flex-col items-center justify-between py-10 lg:gap-0 md:gap-0 sm:gap-0 gap-5">
                     <div>
                         <p>2024 Workflow, LLC. All rights reserved</p>
                     </div>
-                    <div className="flex items-center gap-10">
-                        <RiTwitterXLine size={25} color="gray"/>
-                        <FaTwitch size={25} color="gray"/>
-                        <FaGithub size={25} color="gray"/>
-                        <FaYoutube size={25} color="gray"/>
-                        <FaInstagram size={25} color="gray"/>
+                    <div className="flex items-center lg:gap-10 md:gap-5 sm:gap-5 gap-5">
+                        <RiTwitterXLine size={25} color="gray" />
+                        <FaTwitch size={25} color="gray" />
+                        <FaGithub size={25} color="gray" />
+                        <FaYoutube size={25} color="gray" />
+                        <FaInstagram size={25} color="gray" />
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
