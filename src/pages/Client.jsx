bbfa9904bd2c3150d324d42831becae89b564db5
sohunz <sohunily@gmail.com>
2024-01-31 08:45:19 +0700

    responsive client page

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,34 +1,38 @@
 const Client = () => {
     return (
-        <div className=" max-w-[1440px] flex flex-col justify-center  mx-auto">
-            <div className="text-center">
-                <p className="text-5xl font-bold py-5">
-                    Trusted by developers across the world
-                </p>
-                <p className="text-3xl py-5 mb-5 text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Impedit sunt voluptates nulla. Corporis delectus voluptate
-                    quod modi ipsum sed sunt?
-                </p>
-            </div>
-            <div className="grid grid-cols-3 gap-5">
-                <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
-                    <p className="text-5xl font-extrabold text-blue-700">
-                        100%
+        <div className="w-full px-10">
+            <div className="max-w-[1440px] flex flex-col justify-center  mx-auto">
+                <div className="text-center">
+                    <p className="lg:text-4xl md:text-3xl sm:text-3xl text-3xl font-bold lg:py-5 dm:py-3 sm:py-3 py-3">
+                        Trusted by developers across the world
                     </p>
-                    <p className="text-gray-500">Completion</p>
+                    <p className="lg:text-2xl md:text-xl sm:text-xl text-lg py-5 mb-10 text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Accusantium ab eum rerum, impedit optio eveniet
+                        nisi consectetur iste iusto officia, eos libero illo
+                        molestias laboriosam, ducimus nesciunt ad qui
+                        exercitationem!
+                    </p>
                 </div>
-                <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
-                    <p className="text-5xl font-extrabold text-blue-700">
-                        24/7
-                    </p>
-                    <p className="text-gray-500">Delivery</p>
-                </div>
-                <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
-                    <p className="text-5xl font-extrabold text-blue-700">
-                        100K
-                    </p>
-                    <p className="text-gray-500">Transactions</p>
+                <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 grid-cols-1 lg:gap-5 md:gap-5 sm:gap-2 gap-5">
+                    <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
+                        <p className="lg:text-5xl md:text-4xl sm:text-4xl text-4xl font-extrabold text-blue-800">
+                            100%
+                        </p>
+                        <p className="text-gray-500">Completion</p>
+                    </div>
+                    <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
+                        <p className="lg:text-5xl md:text-4xl sm:text-4xl text-4xl font-extrabold text-blue-800">
+                            24/7
+                        </p>
+                        <p className="text-gray-500">Delivery</p>
+                    </div>
+                    <div className="border flex flex-col justify-center items-center p-10 gap-3 rounded-lg">
+                        <p className="lg:text-5xl md:text-4xl sm:text-4xl text-4xl font-extrabold text-blue-800">
+                            100K
+                        </p>
+                        <p className="text-gray-500">Transactions</p>
+                    </div>
                 </div>
             </div>
         </div>
